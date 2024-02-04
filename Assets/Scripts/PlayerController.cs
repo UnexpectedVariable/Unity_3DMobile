092bee532c5ca4dde16789d141babb07ff92894f
UnexpectedVariable <rodionp49@gmail.com>
2024-02-04 18:37:47 +0200

    Animation and movement
    -Added idle and run animations
    -Changed how movement input is handled
    -Scene changes

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,6 +8,8 @@ using UnityEngine.InputSystem;
 public class PlayerController : MonoBehaviour
 {
     [SerializeField]
+    private Animator _animator;
+    [SerializeField]
     private Rigidbody _rigidbody = null;
     [SerializeField] 
     private PlayerInput _playerInput;
@@ -16,17 +18,26 @@ public class PlayerController : MonoBehaviour
     [SerializeField]
     private GameObject _joystick = null;
 
-    void Update()
+    private void FixedUpdate()
     {
-        
+        Vector3 inputVector = _playerInput.currentActionMap["Move"].ReadValue<Vector2>();
+        //if (inputVector == Vector3.zero) Debug.Log("inputVec is zero");
+
+        inputVector.z = inputVector.y;
+        inputVector.y = 0;
+        _rigidbody.velocity = inputVector * _speedMultiplier;
+        gameObject.transform.rotation = Quaternion.LookRotation(inputVector, Vector3.up);
+
+        //_animator.SetBool("IsMoving", true);
+        _animator.SetFloat("Velocity", _rigidbody.velocity.sqrMagnitude);
     }
 
-    private void OnMove(InputValue value)
+    /*private void OnMove(InputValue value)
     {
-        Debug.Log("On Move");
+        //Debug.Log("On Move");
         Vector3 inputVector = value.Get<Vector2>();
         inputVector.z = inputVector.y;
         inputVector.y = 0;
         _rigidbody.velocity = inputVector * _speedMultiplier;
-    }
+    }*/
 }
